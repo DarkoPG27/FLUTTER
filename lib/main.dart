e829cfa4c89965a03ead6aeb5277e2da1728b0ab
DarkoPG27 <darkobrnovic27@gmail.com>
2023-03-14 13:48:37 +0100

    First Summary & Additional Syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
-addNumbers
+import 'package:flutter/material.dart';
 
-void main() {
+/* void main() {
+  runApp(MyApp());
+} */
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
 }
